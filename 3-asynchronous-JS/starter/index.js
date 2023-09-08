849d0f44d0e99d2003534260354b70096be81eee
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2023-09-08 07:25:06 +0100

    Async await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -21,7 +21,27 @@ const writeFilePro = (file, data) => {
   });
 };
 
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('Random dog image saved to file successfully!😁');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDogPic();
+
 //NOTE: example of chaining then methods on promises
+/*
 readFilePro(`${__dirname}/dog.txt`)
   // readFulePro returns a promise that we can use .then() to resolve
   .then(data => {
@@ -40,3 +60,4 @@ readFilePro(`${__dirname}/dog.txt`)
   .catch(err => {
     console.log(err);
   });
+*/
