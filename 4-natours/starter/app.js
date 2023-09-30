ea164cc4ee0f89d7403fd379859940d7ebf2ee5e
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2023-09-30 11:15:05 +0100

    Get/Post
    - Route Handling
    - Using Middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,15 +1,79 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// Middleware stands between the request and the response.
+// This middleware is responsible for adding the data to the response object and returning it.
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// Route handler for /api/v1/tours endpoint
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint');
+// Variables in the url :id is a parameter that we define
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  // creating new id
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
