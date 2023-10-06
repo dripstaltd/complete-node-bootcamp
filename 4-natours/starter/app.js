f8ce5a82ee20b391c7a1c54c491a947ae4042266
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2023-10-06 09:39:01 +0100

    Update app.js

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -7,54 +7,43 @@ const app = express();
 // This middleware is responsible for adding the data to the response object and returning it.
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// Route handler for /api/v1/tours endpoint
-app.get('/api/v1/tours', (req, res) => {
+/*◙◙◙◙◙ Start of Route Handlers ◙◙◙◙◙*/
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
-      tours: tours,
+      tours,
     },
   });
-});
+};
 
-// Variables in the url :id is a parameter that we define
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
-  // if (id > tours.length) {
+  // no tour is found? error is returned with 404 error code
   if (!tour) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
   }
-
+  // if tour is found then:
   res.status(200).json({
     status: 'success',
     data: {
       tour,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   // console.log(req.body);
   // creating new id
   const newId = tours[tours.length - 1].id + 1;
@@ -74,9 +63,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -87,8 +76,40 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
 
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
+/*◙◙◙◙◙ Route Handlers End ◙◙◙◙◙*/
+/*◙◙◙◙◙ Routes ◙◙◙◙◙*/
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+/*◙◙◙◙◙ Start Server ◙◙◙◙◙*/
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
